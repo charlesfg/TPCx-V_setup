4c52ee59745dfa53e7ad8603b1ebdce5eff4df58
Charles Goncalves <charles.ferreira@miningmath.com>
2016-11-18 08:48:16 -0200

    changing the create_database for a more generic way

--- create_database.sh
+++ create_database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-sed -i "s/Scaling\[1-4\]/Scaling[1-$(hostname | cut -c6)]/" /opt/VDb/pgsql/scripts/linux/env.sh
+sed -i "s/Scaling\[1-[0-4]\]/Scaling[1-$(hostname | cut -c6)]/" /opt/VDb/pgsql/scripts/linux/env.sh
 cd /opt/VDb/pgsql/scripts/linux
 ./setup.sh
 cd -
