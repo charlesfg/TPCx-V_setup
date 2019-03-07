0e7e85b59717a5fba3f8491fcd5ee73c8fd3f5a5
Charles Goncalves <charles.fg@gmail.com>
2019-03-07 12:50:55 -0500

    fixing some errors part 2

--- create_database.sh
+++ create_database.sh
@@ -3,7 +3,8 @@
 sed -i "s/Scaling\[1-[0-4]\]/Scaling[1-$(hostname | cut -c6)]/" /opt/VDb/pgsql/scripts/linux/env.sh
 cat /opt/VDb/pgsql/scripts/linux/env.sh
 cd /opt/VDb/pgsql/scripts/linux
-bash setup.sh 2>&1 | tee  $(hostname)-setupDB-$(date +"%Y%m%d_%H%M").log &
+chmod +x *.sh
+./setup.sh 2>&1 | tee  $(hostname)-setupDB-$(date +"%Y%m%d_%H%M").log &
 echo "Setup finished at $(date +"%Y%m%d_%H%M")"
 cd -
 
