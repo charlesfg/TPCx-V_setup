280d49ce6e94fc6df49aecd2fd0e3b82ceef77ab
Charles Goncalves <charles.fg@gmail.com>
2020-10-26 14:53:42 -0200

    checkpoiint Mon Oct 26 14:53:42 BRST 2020

---
+++ results_scripts/prepare_environment_test.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# Usage :
+# $1 ->target filter
+# $2 ->directory target
+# $3 ->files
+function copyTo(){
+    TARGET=$1
+    shift;
+    TARGET_DIR=$1
+    shift;
+    for i in `cat /etc/hosts | grep $TARGET | awk '{print $2}'`; do
+        echo $i;
+        scp $@ ${i}:${TARGET_DIR}
+    done
+}
+
+
+alias copytoall="copyTo tpc-g"
+alias copytoa="copyTo tpc-g[0-9]a"
+alias copytob="copyTo tpc-g[0-9]b"
+
+
+function runAt(){
+    TARGET=$1
+    shift;
+    for i in `cat /etc/hosts | grep $TARGET | awk '{print $2}'`; do
+        echo $i;
+        ssh $i $@;
+    done
+}
+
+
+alias runatall="runAt tpc-g"
+alias runata="runAt tpc-g[0-9]a"
+alias runatb="runAt tpc-g[0-9]b"
+
+SECONDS=0
+
+# clean all caches
+copyTo tpc-g ~ clean_caches.sh
+
+# ensure that the VDriver remote dir is mounted
+./ensure_mounted_runs_dir.sh
+
+ssh tpc-tenant 'find ~ -name tc_tester\*.log -exec rm  {} \; '
+
+runAt tpc-g 'bash ~/clean_caches.sh'
+runAt tpc-g 'test -e /dbstore/backup && rm -rf /dbstore/backup/*'
+runAt tpc-g 'test -e /dbstore/tpcv-data/pg_log && rm -rf /dbstore/tpcv-data/pg_log/*'
+runAt tpc-g 'psql -U tpcv -c VACUUM'
+
+ssh tpc-driver 'cd /opt/runs && bash -x kill_run.sh'
+
+
+# Start the load on the tpc-tenant performing a cleaning before
+ssh tpc-tenant screen -d -m ./kill_background_work.sh
+ssh tpc-tenant2 screen -d -m ./kill_background_work.sh
+ssh tpc-tenant3 screen -d -m ./kill_background_work.sh
+
+echo $(date +%Y-%m-%d_%H%M%S) >> /root/perftest
+CNT=$(wc -l /root/perftest | awk '{print $1}' )
+LABEL="NONE"
+case $CNT  in
+    1)
+        LABEL="CPU tenant[1]"
+        echo $LABEL
+        ssh tpc-tenant screen -d -m ./run_background_work_tests.sh 2
+        ;;
+    2)
+        LABEL="IO tenant[1]"
+        echo $LABEL
+        ssh tpc-tenant screen -d -m ./run_background_work_tests.sh 1
+        ;;
+    3)
+        LABEL="CPU tenant[1,2]"
+        echo $LABEL
+        ssh tpc-tenant screen -d -m ./run_background_work_tests.sh 2
+        ssh tpc-tenant2 screen -d -m ./run_background_work_tests.sh 2
+        ;;
+    4)
+        LABEL="CPU tenant[1,2,3]"
+        echo $LABEL
+        ssh tpc-tenant screen -d -m ./run_background_work_tests.sh 2
+        ssh tpc-tenant2 screen -d -m ./run_background_work_tests.sh 2
+        ssh tpc-tenant3 screen -d -m ./run_background_work_tests.sh 2
+        ;;
+    5)
+        LABEL="IO em tenants[1,2]"
+        echo $LABEL
+        ssh tpc-tenant screen -d -m ./run_background_work_tests.sh 1
+        ssh tpc-tenant2 screen -d -m ./run_background_work_tests.sh 1
+        ;;
+    *)
+        echo "Cabo ... "
+esac
+echo $LABEL > /tmp/run_alias
+# do some work
+duration=$SECONDS
+echo "$(($duration / 60)):$(($duration % 60)) $((hostname))" 
